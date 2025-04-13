ef6c445830c4a1e2b639b797f215b696ebb09f3b
Hileluce <126271686+Hileluce@users.noreply.github.com>
2025-04-13 22:17:57 +0400

    settings warning

--- Assets/__Scripts/LoadingScreen.cs
+++ Assets/__Scripts/LoadingScreen.cs
@@ -2,15 +2,37 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class LoadingScreen : MonoBehaviour
 {
-   
+    public GameObject memeSettingImage;
+    public GameObject settingWarning;
+    bool memeWasShown;
 
     public void StartGame() 
     {
         SceneManager.LoadScene(1);
+        memeWasShown = false;
     }
 
     public void QuitGame()
     {
         Application.Quit();
     }
+    public void ActivateMemeImage()
+    {
+        if(!memeWasShown) 
+        {
+            memeSettingImage.SetActive(true);
+            Invoke("DisableMemeImage", 0.5f);
+            memeWasShown = true;
+        }
+        else
+        {
+            settingWarning.SetActive(true);
+        }
+        
+    }
+    void DisableMemeImage()
+    {
+        memeSettingImage.SetActive(false);
+    }
+
 }
